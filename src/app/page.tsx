fc6ee15f4d28bea24995ae14b4c8ccecde6e305a
Milaadmg023 <bagerim962@gmail.com>
2025-05-27 10:42:16 +0330

    Add additional components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+// Components
 import Header from "@/components/home/header"
+import Features from "@/components/home/features"
+import HowItWorks from "@/components/home/how_it_works"
+import GetStart from "@/components/home/get_start"
+import FAQ from "@/components/home/faq"
+import Footer from "@/components/home/footer"
+import About from "@/components/home/about"
+import Pricing from "@/components/home/pricing"
+import Testimonials from "@/components/home/testimonials"
 
 export default function Home() {
     return(
